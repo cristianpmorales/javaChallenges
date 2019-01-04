d4088fe56bec37bee894fa6aba116f4dfd51327b
cristianpmorales <39247282+cristianpmorales@users.noreply.github.com>
2019-01-04 14:19:58 -0500

    Add files via upload

---
+++ ReadingUserInputChallenge/src/com/company/Main.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+//    -Read 10 numbers from the console entered by the user and print the sum of those numbers.
+//    -Create a Scanner like we did in the previous video.
+//    -Use the hasNextInt() method from the scanner to check if the user has entered an int value.
+//    -If hasNextInt() returns false, print the message gInvalid Numberh. Continue reading until you have read 10 numbers.
+//    -Use the nextInt() method to get the number and add it to the sum.
+//    -Before the user enters each number, print the message gEnter number #x:h where x represents the count, i.e. 1, 2, 3, 4, etc.
+//    -For example, the first message printed to the user would be gEnter number #1:h, the next gEnter number #2: h, and so on.
+//
+//    Hint:
+//    -Use a while loop.
+//    -Use a counter variable for counting valid numbers.
+//    -Close the scanner after you donft need it anymore.
+//    -Create a project with the name ReadingUserInputChallenge.
+
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner =  new Scanner(System.in);
+        int count = 1;
+        int totalSum = 0;
+        while (count < 11){
+
+            System.out.println("Enter a Number pls");
+
+            boolean hasNextInt = scanner.hasNextInt();
+
+            if (hasNextInt){
+                int numberEntered = scanner.nextInt();
+                System.out.println("Enter number # "+ count +": " + numberEntered);
+                totalSum += numberEntered;
+                count++;
+            } else {
+                System.out.println("Not a number ");
+//                instead of using the break using the scanner.nextline
+//                 break;
+            }
+            scanner.nextLine();
+        }
+        System.out.println("The total is "  + totalSum);
+        scanner.close();
+    }
+}
